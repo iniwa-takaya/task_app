13056328cf639cb748aadb3529873a1b212b8b86
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-03 23:18:28 +0900

    新規登録/ログイン/ログアウト機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,11 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :nick_name
+    validates :name, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ } 
+    validates :birthday
+  end
+  validates :password, format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i }
 end
