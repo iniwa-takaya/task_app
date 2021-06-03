f6bcbd73b8c79ba0b608f33e8b5cfeb9f40c5736
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-03 23:46:19 +0900

    roomsテーブルの作成

---
+++ db/migrate/20210603143858_create_spaces.rb
@@ -0,0 +1,9 @@
+class CreateSpaces < ActiveRecord::Migration[6.0]
+  def change
+    create_table :spaces do |t|
+      t.string :name,     null: false
+      t.references :user, foreign_key: true
+      t.timestamps
+    end
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_03_135642) do
+ActiveRecord::Schema.define(version: 2021_06_03_143858) do
+
+  create_table "spaces", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "name", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_spaces_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "nick_name", null: false
@@ -27,4 +35,5 @@ ActiveRecord::Schema.define(version: 2021_06_03_135642) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "spaces", "users"
 end
