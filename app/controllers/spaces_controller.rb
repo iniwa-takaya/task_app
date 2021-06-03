6e8ecd16e7b713cf723986dd2596ed8faa49f5c9
iniwa-takaya <83565251+iniwa-takaya@users.noreply.github.com>
2021-06-04 00:30:16 +0900

    スペース保存機能の実装

--- app/controllers/spaces_controller.rb
+++ app/controllers/spaces_controller.rb
@@ -1,8 +1,27 @@
 class SpacesController < ApplicationController
+  before_action :show_spaces, only: %i[index new]
+
   def index
   end
   def new
+    @space = Space.new
   end
   def create
+    @space = Space.new(space_params)
+    if @space.save
+      redirect_to new_space_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def space_params
+    params.require(:space).permit(:name).merge(user_id: current_user.id)
+  end
+
+  def show_spaces
+    @spaces = Space.all.order('created_at DESC')
   end
 end
